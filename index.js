d5efe9916e2f9f00204a9ff51e754954d0c4fd8e
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-05-10 11:33:15 +0100

    new variables added

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var products_1 = require("./products");
 var productName = 'beanie';
+var shipping;
+var taxPercent;
+var taxTotal;
+var total;
 var product = products_1.default.filter(function (product) {
     return product.name === productName;
 })[0];
-// console.log(product)
+// console.log(typeof Boolean(product.preOrder))
 if (Boolean(product.preOrder) === true) {
     console.log("We will send you a message when your product is on its way.");
 }
--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import products from "./products";
 
 const productName: String = 'beanie';
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
 
 const product = products.filter((product) => 
     product.name === productName)[0];
 
-// console.log(product)
+// console.log(typeof Boolean(product.preOrder))
 
 if (Boolean(product.preOrder) === true){
     console.log("We will send you a message when your product is on its way.")
